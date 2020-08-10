45389c7bac755ecc23e9bb0d960de14205f32c40
Разиков Алексей <razikov88@yandex.ru>
2020-08-10 22:23:03 +0300

    fix

--- mysql/select.sql
+++ mysql/select.sql
@@ -2,5 +2,5 @@ use dikidi;
 SELECT t.name, count(m.id) as 'количество'
 FROM `motorcycles` as m
 LEFT JOIN `types` as t ON m.type_id = t.id 
-WHERE m.in_production = 0
-GROUP BY m.type_id
+WHERE m.in_production = 1
+GROUP BY t.name
